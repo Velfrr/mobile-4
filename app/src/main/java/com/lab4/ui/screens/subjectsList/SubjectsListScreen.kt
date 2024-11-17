f18df651b22b650c7802f07515dc91f4ed0e2c2d
Andrii Lukashevych <andriy.lukashewwich@gmail.com>
2024-11-17 22:05:50 +0200

    initial commit

---
+++ app/src/main/java/com/lab4/ui/screens/subjectsList/SubjectsListScreen.kt
@@ -0,0 +1,75 @@
+package com.lab4.ui.screens.subjectsList
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.lab4.data.db.DatabaseStorage
+import com.lab4.data.entity.SubjectEntity
+
+@Composable
+fun SubjectsListScreen(onDetailsScreen: (Int) -> Unit) {
+    val context = LocalContext.current
+    val db = DatabaseStorage.getDatabase(context)
+    val subjectsListState = remember { mutableStateOf<List<SubjectEntity>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        subjectsListState.value = db.subjectsDao.getAllSubjects()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(10.dp)
+    ) {
+        Text(
+            text = "Список предметів",
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 15.dp).align(Alignment.CenterHorizontally)
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(subjectsListState.value) { subject ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                        .clickable { onDetailsScreen(subject.id) },
+                    shape = MaterialTheme.shapes.small,
+                ) {
+                    Column(
+                        modifier = Modifier.padding(12.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start,
+                    ) {
+                        Text(
+                            text = subject.title,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.padding(bottom = 6.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
